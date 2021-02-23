686e2a9723851b28f9c6fd7e3d5b560eff6c5ed1
Pavel <whatisclamp@gmail.com>
2021-02-23 18:01:03 +0200

    Add privacy

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
   closeElem = document.querySelector(".menu__close"),
-  overlay = document.querySelector(".menu__overlay");
+  overlay = document.querySelector(".menu__overlay"),
+  links = document.querySelectorAll(".menu__link a");
 
 hamburger.addEventListener("click", () => {
   menu.classList.add("active");
@@ -15,9 +16,26 @@ overlay.addEventListener("click", () => {
   menu.classList.remove("active");
 });
 
+links.forEach((i) => {
+  i.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+});
+
 const percent = document.querySelectorAll('.skills__percents-percent'),
   lines = document.querySelectorAll('.skills__percents-line span');
 
 percent.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
+});
+
+const arrow = document.querySelector('.arrow_up');
+
+arrow.onclick = function () {
+  window.scrollTo(pageXOffset, 0);
+  // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
+};
+
+window.addEventListener('scroll', function () {
+  arrow.hidden = (pageYOffset < document.documentElement.clientHeight);
 });
